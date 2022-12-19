458a81a0a8b22f9a4a48dee3457f72f896d7f439
KCaverly <kylebcaverly@gmail.com>
2022-12-19 12:29:21 -0500

    updated projects to include max_depth, remove duplicates

--- src/projects.rs
+++ src/projects.rs
@@ -1,32 +1,64 @@
 use skim::prelude::*;
-use std::fs;
 use std::io;
 use std::io::Cursor;
-use std::path;
+use walkdir::{DirEntry, WalkDir};
 
-fn folders(dir: &path::Path) -> Result<Vec<path::PathBuf>, io::Error> {
-    Ok(fs::read_dir(dir)?
+fn folders(path: &str, max_depth: u8) -> Result<Vec<DirEntry>, io::Error> {
+    Ok(WalkDir::new(path)
+        .min_depth(max_depth.into())
+        .max_depth(max_depth.into())
         .into_iter()
-        .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
-        .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
-        .filter(|r| r.is_dir()) // Filter out non-folders
-        .collect())
+        .filter(|x| x.is_ok())
+        .map(|r| r.unwrap())
+        .filter(|x| x.path().is_dir())
+        .collect::<Vec<DirEntry>>())
+}
+
+struct ParentDirectory {
+    path: String,
+    include_hidden: bool,
+    max_depth: u8,
 }
 
 pub fn get_files() -> Vec<String> {
-    let directory_list = vec!["/home/kcaverly/personal"];
+    let directory_list = vec![
+        ParentDirectory {
+            path: "/home/kcaverly/personal".to_string(),
+            include_hidden: false,
+            max_depth: 1,
+        },
+        ParentDirectory {
+            path: "/home/kcaverly/.dotfiles".to_string(),
+            include_hidden: true,
+            max_depth: 3,
+        },
+        ParentDirectory {
+            path: "/home/kcaverly/work".to_string(),
+            include_hidden: true,
+            max_depth: 2,
+        },
+    ];
+
     let mut file_list: Vec<String> = Vec::new();
 
-    for directory_path in directory_list.iter() {
-        let dir_files: Vec<String> = folders(path::Path::new(directory_path))
+    for directory in directory_list.iter() {
+        let dir_files: Vec<String> = folders(&directory.path, directory.max_depth)
             .unwrap()
             .iter()
-            .map(|x| x.as_path().to_str().unwrap().to_string())
-            .filter(|x| !x.starts_with("."))
+            .map(|x| x.path().to_str().unwrap().to_string())
+            .filter(|x| {
+                if directory.include_hidden {
+                    true
+                } else {
+                    !x.starts_with(".")
+                }
+            })
             .collect();
 
         for file in dir_files.iter() {
-            file_list.push(file.to_string());
+            if file.to_string() != directory.path {
+                file_list.push(file.to_string());
+            }
         }
     }
 
@@ -47,5 +79,9 @@ pub fn search_options(search_options: Vec<String>) -> String {
         .map(|out| out.selected_items)
         .unwrap_or_else(|| Vec::new());
 
-    return selected_item.first().expect("No Option Selected!").output().to_string();
+    return selected_item
+        .first()
+        .expect("No Option Selected!")
+        .output()
+        .to_string();
 }
