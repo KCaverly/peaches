6b1364a75700aadd5db62cd10d833f0274535e5b
KCaverly <kylebcaverly@gmail.com>
2022-12-21 12:50:43 -0500

    moved install.sh to remove existing version of peaches

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Removing Old Version of Peaches
+rm -rf ~/.local/bin/peaches
+
 # Get Latest Release from Github
 wget https://github.com/KCaverly/peaches/releases/download/v0.2.0/peaches --no-check-certificate -P ~/.local/bin/
 chmod +x ~/.local/bin/peaches
