e13e4b498f90d14766f4bcfc4d13655ce1b9c6e1
KCaverly <kylebcaverly@gmail.com>
2023-01-08 13:56:15 -0500

    added initial tasks command

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,13 +1,15 @@
-use crate::{config::{Config}, tmux::TMUX};
+use crate::{config::Config, tmux::TMUX};
 
 pub struct TasksCommand {}
 
 impl TasksCommand {
     pub fn run(cfg: &Config) {
-
-        TMUX::create_window("tasks", "tasks");
-        TMUX::send_keys("tasks", "tasks", "taskwarrior-tui");
-        TMUX::attach_or_select_window("tasks", "tasks");
-
+        if TMUX::window_exists("tasks", "tasks") {
+            TMUX::attach_or_select_window("tasks", "tasks");
+        } else {
+            TMUX::create_window("tasks", "tasks");
+            TMUX::send_keys("tasks", "tasks", "taskwarrior-tui");
+            TMUX::attach_or_select_window("tasks", "tasks");
+        }
     }
 }
