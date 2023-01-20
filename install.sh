94f8790fcd1793f5368c9f8df677565632f26c87
KCaverly <kylebcaverly@gmail.com>
2023-01-20 11:17:42 -0500

    release v0.4.6

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 rm -rf ~/.local/bin/peaches
 
 # Get Latest Release from Github
-wget https://github.com/KCaverly/peaches/releases/download/v0.4.5/peaches --no-check-certificate -P ~/.local/bin/
+wget https://github.com/KCaverly/peaches/releases/download/v0.4.6/peaches --no-check-certificate -P ~/.local/bin/
 chmod +x ~/.local/bin/peaches
